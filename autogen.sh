88fd9ce5fbeb350fa95b6c9889ae8cf1ae496c5a
Paul Querna <chip@outoforder.cc>
2004-04-10 20:55:55 +0000

    hook in libxslt checking

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # autogen.sh - generates configure using the autotools
 # $Id: autogen.sh,v 1.1 2004/03/04 08:12:13 firechipmunk Exp $
+export WANT_AUTOCONF=2.5
 libtoolize --force --copy
 #libtoolize14  --force --copy
 aclocal -I m4
