d736308dc9fd7080a79ec39a353d49f6ce3d1433
Edward Rudd <urkle@outoforder.cc>
2004-10-16 03:02:36 +0000

    use symbolic names for AM_INIT_AUTOMAKE

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,6 @@
 # $Id: autogen.sh,v 1.1 2004/03/04 08:12:13 firechipmunk Exp $
 export WANT_AUTOCONF=2.5
 libtoolize --force --copy
-#libtoolize14  --force --copy
 aclocal -I m4
 autoheader
 automake --add-missing --copy --foreign
